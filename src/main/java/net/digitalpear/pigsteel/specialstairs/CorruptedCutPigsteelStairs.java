b61517d0e69e980b98afdabd085a21c4ce50ea48
DP <digitalpeargaming@gmail.com>
2021-05-30 10:41:27 -0700

    Update CorruptedCutPigsteelStairs.java

--- src/main/java/net/digitalpear/pigsteel/specialstairs/CorruptedCutPigsteelStairs.java
+++ src/main/java/net/digitalpear/pigsteel/specialstairs/CorruptedCutPigsteelStairs.java
@@ -2,7 +2,6 @@ package net.digitalpear.pigsteel.specialstairs;
 
 import net.digitalpear.pigsteel.PigsteelMod;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
-import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.StairsBlock;
