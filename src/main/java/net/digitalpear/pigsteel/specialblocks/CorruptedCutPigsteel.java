8aaf965a7f4829a448635bbaf9dc183f7833997b
DP <digitalpeargaming@gmail.com>
2021-05-30 10:39:58 -0700

    Blocks can now be waxed by right clicking with honeycomb

--- src/main/java/net/digitalpear/pigsteel/specialblocks/CorruptedCutPigsteel.java
+++ src/main/java/net/digitalpear/pigsteel/specialblocks/CorruptedCutPigsteel.java
@@ -5,9 +5,17 @@ import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Items;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 import java.util.Random;
 
@@ -24,5 +32,12 @@ public class CorruptedCutPigsteel extends Block{
                 world.setBlockState(pos, PigsteelMod.ZOMBIFIED_CUT_PIGSTEEL.getDefaultState());
             }
         }
+    }
+    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit){
+        if (player.getStackInHand(hand).getItem() == Items.HONEYCOMB) {
+            world.setBlockState(pos, PigsteelMod.WAXED_CORRUPTED_CUT_PIGSTEEL.getDefaultState());
+            world.playSound((PlayerEntity)null, pos, SoundEvents.BLOCK_HONEY_BLOCK_PLACE, SoundCategory.BLOCKS, 1.0F, 0.8F + world.random.nextFloat() * 0.4F);
+        }
+        return ActionResult.PASS;
     }
 }
