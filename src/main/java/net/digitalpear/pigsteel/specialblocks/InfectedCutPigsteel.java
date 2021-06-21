4e8c2364dfeb6511bc627ccaa187c02fc4ed264b
Uraneptus <50745606+Uraneptus333@users.noreply.github.com>
2021-06-21 19:15:13 +0200

    Update InfectedCutPigsteel.java

--- src/main/java/net/digitalpear/pigsteel/specialblocks/InfectedCutPigsteel.java
+++ src/main/java/net/digitalpear/pigsteel/specialblocks/InfectedCutPigsteel.java
@@ -35,7 +35,7 @@ public class InfectedCutPigsteel extends Block{
     }
     public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit){
         if (player.getStackInHand(hand).getItem() == Items.HONEYCOMB) {
-            world.setBlockState(pos, PigsteelMod.WAXED_CORRUPTED_CUT_PIGSTEEL.getDefaultState());
+            world.setBlockState(pos, PigsteelMod.WAXED_INFECTED_CUT_PIGSTEEL.getDefaultState());
             world.playSound((PlayerEntity)null, pos, SoundEvents.BLOCK_HONEY_BLOCK_PLACE, SoundCategory.BLOCKS, 1.0F, 0.8F + world.random.nextFloat() * 0.4F);
         }
         return ActionResult.PASS;
