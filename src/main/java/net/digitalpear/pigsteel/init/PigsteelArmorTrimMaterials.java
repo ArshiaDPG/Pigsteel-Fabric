28e1b8f5e011199e5f73f1f8fb7c29b0c878db47
DP <digitalpeargaming@gmail.com>
2025-02-04 10:09:46 -0800

    port to 1.21.4 (broken trims)

--- src/main/java/net/digitalpear/pigsteel/init/PigsteelArmorTrimMaterials.java
+++ src/main/java/net/digitalpear/pigsteel/init/PigsteelArmorTrimMaterials.java
@@ -19,12 +19,10 @@ public class PigsteelArmorTrimMaterials {
 
 
     public static void bootstrap(Registerable<ArmorTrimMaterial> registry) {
-        register(registry, PIGSTEEL, PigsteelItems.PIGSTEEL_CHUNK, Style.EMPTY.withColor(11898803), 1.0F);
+        register(registry, PIGSTEEL, PigsteelItems.PIGSTEEL_CHUNK, Style.EMPTY.withColor(11898803));
     }
 
-    private static void register(Registerable<ArmorTrimMaterial> registry, RegistryKey<ArmorTrimMaterial> key, Item ingredient, Style style, float itemModelIndex) {
-        register(registry, key, ingredient, style, Map.of());
-    }
+
     private static void register(Registerable<ArmorTrimMaterial> registry, RegistryKey<ArmorTrimMaterial> key, Item ingredient, Style style) {
         register(registry, key, ingredient, style, Map.of());
     }
